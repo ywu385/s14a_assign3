ca6e6dbf26021877dec71f88af95cd5a67a64bb7
John Wu <yuw385@g.harvard.com>
2023-07-23 19:56:54 -0700

    version 1 commit

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
 from flask import Flask, request, render_template, redirect
 import json
 from flask_sqlalchemy import SQLAlchemy
+# from db_init import User
+from sqlalchemy import func
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://doadmin:password@db-postgresql-s14a-do-user-14294326-0.b.db.ondigitalocean.com:25060/s14a'
+postgres_string = 'postgresql://doadmin:AVNS_B3PZmgQl943DmavUsNZ@db-postgresql-s14a-do-user-14294326-0.b.db.ondigitalocean.com:25060/s14a?sslmode=require'
+app.config['SQLALCHEMY_DATABASE_URI'] = postgres_string
 db.init_app(app)
-# # db = sqlalchemy(app)
 
-# engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key= True, autoincrement = True)
+    email = db.Column(db.String)
+    phone_number = db.Column(db.String)
+    updated_at = db.Column(db.DateTime, default = func.now(), onupdate=func.now())
+    status = db.Column(db.Integer)
+    is_admin = db.Column(db.Boolean)
 
-# try:
-#     with engine.connect() as connection:
-#         print("Connection successful!")
-# except Exception as e:
-#     print("Connection failed!")
-#     print(str(e))
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 links = [
     {"label": "Home", "url": "/home"},
-    {"label": "About", "url": "/about"},
+    {"label": "Update Info", "url": "/searchuser"},
     {"label": "Users", "url": "/Users"},
-    {'label':'Register', 'url':'/register'}
+    {'label':'Register', 'url':'/adduser'},
+    {'label':'Remove User', 'url':'/deleteuser'}
 ]
 
 app.debug = True
@@ -33,63 +39,111 @@ def index():
     return render_template('index.html',navigation=links, header='Home')
     # return render_template('test.html')
 
-
-@app.route("/home")
-def home():
-    return render_template('index.html',navigation = links, header='Home')
-
-@app.route("/about")
-def about():
-    return render_template('about.html',navigation = links, header='About')
-
 @app.route("/Users")
-def list():
-    with open('data/comment_table.json','r') as f:
-        data = json.load(f)
-    return render_template('table.html',data = data, header = 'Who has been here?')
+def userlist(): 
+    with app.app_context():
+        userslist = db.session.execute(
+            db.select(User).order_by(User.id)).scalars()
+        return render_template('table.html',users= userslist,navigation = links, header = 'Users')
+
+@app.route("/adduser", methods = ['GET','POST'])
+def registration():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        phone_number = request.form.get('phone_number')  
+    
+        with app.app_context():
+            existing_user = User.query.filter((User.email == email) | (User.phone_number == phone_number)).first()
+            if existing_user is not None:
+               return "User with this email or phone number already exists!", 400
+
+            user = User(email=email,
+               phone_number=phone_number,
+               status = 1,
+               is_admin=False)
+           
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect('/success')
+    
+    else:
+         
+        return render_template('adduser.html', navigation = links, header='Registration', message = 'Please Sign Up')
+    
+@app.route('/deleteuser', methods=['GET','POST'])
+def deleteuser():
+    if request.method == 'POST':
+        user_id = request.form.get('UserId')
+
+        with app.app_context():
+            user = User.query.get(user_id)
+            if user:
+                db.session.delete(user)
+                db.session.commit()
+                return redirect('/Users')
+            else:
+                return 'User not found!', 404
+    else:
+        return render_template('deleteuser.html', navigation = links, header = 'Delete User')
+
+
+@app.route("/searchuser", methods=['GET', 'POST'])
+def searchuser():
+    if request.method == 'POST':
+        email = request.form.get('email')
+
+        with app.app_context():
+            user = User.query.filter_by(email=email).first()
+
+            if user is None:
+                return redirect('/adduser?message=User+Email+not+found%2C+please+sign+up')
+            else:
+                return redirect('/updateuser/' + str(user.id))
+
+    else:
+        return render_template('finduser.html', navigation = links, header='Search User')
+
+
+@app.route("/updateuser/<int:user_id>", methods = ['GET','POST'])
+def updateuser(user_id):
+    with app.app_context():
+        user = User.query.get(user_id) # query for user
+
+        if request.method == 'POST':
+            email = request.form.get('email')
+            phone_number = request.form.get('phone_number') 
+            is_admin = request.form.get('is_admin')
+            if is_admin =='yes':
+                is_admin = True
+            else:
+                is_admin = False 
+            
+            # Update user's information
+            user.phone_number = phone_number
+            user.is_admin = is_admin
+            
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect('/Users')
+        
+        else:
+            
+            return render_template('updateuser.html', navigation = links, user = user, header='Update User')
+
+
+
 
 @app.route('/success')
 def success():
     return render_template('success.html',header='Success!')
 
-@app.route("/register", methods = ['GET','POST'])
-def contact():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        comments = request.form.get('phone_number')
 
-        data = {'email':email,
-                'phone_number':phone_number}
-        
-     # Append into database here
-        return redirect('/success')
-    
-    return render_template('Form.html', header='Register')
+@app.route("/home")
+def home():
+    return render_template('index.html',navigation = links, header='Home')
 
-@app.route("/registration", methods = ['GET','POST'])
-def registration():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        data = {'email': email,
-                'password': password}
-     
-         # Adding data into JSON file
-        with open('data/registration.json', 'a+') as f:
-            f.seek(0)
-            try:
-                old_data = json.load(f)
-            except ValueError:
-                old_data = []
-
-            old_data.append(data)
-
-        with open('data/registration.json','w') as f:
-            json.dump(old_data,f)
-
-        return redirect('/home')
-    return render_template('registration.html', header='Registration')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5002)
--- db_init.py
+++ db_init.py
@@ -1,24 +1,58 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-app = Flask(__name__)
-postgres_string = 'postgresql://doadmin:AVNS_B3PZmgQl943DmavUsNZ@db-postgresql-s14a-do-user-14294326-0.b.db.ondigitalocean.com:25060/s14a?sslmode=require'
-app.config['SQLALCHEMY_DATABASE_URI'] = postgres_string
-db.init_app(app)
-
-# Here's the code to test the connection:
-with app.app_context():
-    try:
-        with db.engine.connect():
-            print("Database connection successful!")
-    except Exception as e:
-        print("Database connection failed!")
-        print(str(e))
+db=SQLAlchemy()
 
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_keys= True, nullable = False)
-    username = db.Column(db.String)
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key= True, autoincrement = True)
     email = db.Column(db.String)
-    phonenumber = db.Column(db.String)
+    phone_number = db.Column(db.String)
+    status = db.Column(db.Integer)
+    is_admin = db.Column(db.Boolean)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+def main():
+    app = Flask(__name__)
+    postgres_string = 'postgresql://doadmin:AVNS_B3PZmgQl943DmavUsNZ@db-postgresql-s14a-do-user-14294326-0.b.db.ondigitalocean.com:25060/s14a?sslmode=require'
+    app.config['SQLALCHEMY_DATABASE_URI'] = postgres_string
+    db.init_app(app)
+
+
+
+    with app.app_context():
+        try:
+            with db.engine.connect():
+                print("Database connection successful!")
+        except Exception as e:
+            print("Database connection failed!")
+            print(str(e))
+
+    with app.app_context():
+        
+        db.create_all()
+
+        users = db.session.execute(
+            db.select(User).order_by(User.id)).scalars()
+        for u in users:
+            print(u.as_dict()
+                )
+
+        user1 = User(
+            phone_number = '1234567890',
+            email = 'email@example.com'
+        )
+        db.session.add(user1)
+        db.session.commit()
+
+        userlist = db.session.execute(
+            db.select(User).order_by(User.id)).scalars()
+        for u in userlist:
+            print(u.as_dict()
+                )
+
+        # user = db.get_or_404(User, id)
+        # print(user.as_dict())
